9b4c440cf7f0f3648e09f3da6645076757d6dc36
susanmarie87 <susan.marie87@gmail.com>
2021-07-08 17:38:14 +0000

    Added routing for htm pages in run.py

--- run.py
+++ run.py
@@ -10,6 +10,25 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+    @app.route("/events")
+def events():
+    return render_template("events.html")
+
+
+    @app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+    @app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
