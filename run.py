2487acedcda046dc31a7c2f7fea5284393124d33
susanmarie87 <susan.marie87@gmail.com>
2021-07-09 15:50:54 +0000

    Applied Jinja to Navbar links and changes the names

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template
 
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -28,6 +28,7 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(
