427d778c299e840ca5fc44099c499ab4bc325c6c
susanmarie87 <susan.marie87@gmail.com>
2021-07-19 13:46:11 +0000

    Add toogle form function to login

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function toggleForm() {
+    var container = document.querySelector('.container');
+    var section = document.querySelector('section');
+    container.classList.toggle('active')
+    section.classList.toggle('active')
+}
