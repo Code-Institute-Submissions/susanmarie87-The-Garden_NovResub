f39fb3151219077a719a4b2dcf6ec8ace39531f3
susanmarie87 <susan.marie87@gmail.com>
2021-07-24 18:57:51 +0000

    defined toggle functio for hamburger menu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,20 @@
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+    window.addEventListener("scroll", function () {
+        var header = document.querySelector("header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
 
-function toggleForm() {
-    var container = document.querySelector('.container');
-    var section = document.querySelector('section');
-    container.classList.toggle('active')
-    section.classList.toggle('active')
-}
+    function toggle() {
+        var header = document.getElementById('header');
+        header.classList.toggle('active')
+    }
+
+    function toggleForm() {
+        var container = document.querySelector('.container');
+        var section = document.querySelector('section');
+        container.classList.toggle('active');
+        section.classList.toggle('active');
+    }
+
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
