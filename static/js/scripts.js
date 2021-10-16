7699ed1aeb3ca7d845b30d37199162f056ea2fcd
susanmarie87 <susan.marie87@gmail.com>
2021-10-16 03:47:16 +0000

    Add JS Contact Form dependencies and functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,37 @@
-    window.addEventListener("scroll", function () {
-        var header = document.querySelector("header");
-        header.classList.toggle("sticky", window.scrollY > 0);
-    });
+import emailjs from 'emailjs.com'
 
-    function toggle() {
-        var header = document.getElementById('header');
-        header.classList.toggle('active');
-    }
+//Contact Message Functionality//
 
-    function toggleForm() {
-        var container = document.querySelector('.container');
-        var section = document.querySelector('section');
-        container.classList.toggle('active');
-        section.classList.toggle('active');
-    }
+function SendEmail(e) {
+    e.preventDefault();
 
-    $('.collapsible').collapsible();
-    $('.modal').modal();
-    $('.dropdown-trigger').dropdown();
+    emailjs.sendForm('gmail', 'gGarden', //form.current
+            e.target, 'user_jNykmAiHAI1nHZ4amoFHp')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    e.target.reset()
+};
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggle() {
+    var header = document.getElementById('header');
+    header.classList.toggle('active');
+}
+
+function toggleForm() {
+    var container = document.querySelector('.container');
+    var section = document.querySelector('section');
+    container.classList.toggle('active');
+    section.classList.toggle('active');
+}
+
+$('.collapsible').collapsible();
+$('.modal').modal();
+$('.dropdown-trigger').dropdown();
